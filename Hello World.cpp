b44afab1f59d4a12b5fe9402e0ca8071a969b983
sibghatullah <stbstudies@gmail.com>
2024-08-30 04:55:39 -0700

    first commit

---
+++ Hello World.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a , b;
+	printf("enter the values");
+
+	scanf("%d" , &a);
+
+	scanf("%d" , &b);
+	int sum = a + b;
+	printf("sum is %d" , &sum);
+	return 0;
+
+	}
